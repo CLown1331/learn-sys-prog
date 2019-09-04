c728c052e27464169bcd9be92d606b3658399541
CLown1331 <clown1331@yahoo.com>
2019-09-05 00:56:35 +0600

    added

---
+++ recursively-list-directory/sol.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main(int argc, char **argv) {
+    DIR *dp;
+
+    struct dirent dirp;
+
+    if (argc != 2) {
+        printf("No args provided!\n");
+        exit(1);
+    }
+
+    if ((dp = opendir(argv[1])) == NULL) {
+        printf("Cannot open directory. Error: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    while((dirp = readdir(dp)) !=  )
+
+    closedir(dp);
+    return 0;
+}
