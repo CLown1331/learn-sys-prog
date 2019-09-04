d224d0d5260412f1a2091a96e8ae626524937904
CLown1331 <clown1331@yahoo.com>
2019-09-04 18:48:51 +0600

    alarm signal

---
+++ alarm_signal/alarm_signal.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void sig_handler(int signum) {
+	printf("Received Signal Number: %d\n", signum);
+	system("/bin/ls");
+	exit(0);
+}
+
+int main() {
+	alarm(5);
+	signal(SIGALRM, &sig_handler);
+
+	for(;;)
+		sleep(1);
+
+	return 0;
+}
