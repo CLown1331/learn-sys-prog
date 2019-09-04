fe82494334aeafe7eee4dc3a45fd35c2ebb262bf
CLown1331 <clown1331@yahoo.com>
2019-09-05 01:12:19 +0600

    added

--- recursively-list-directory/sol.c
+++ recursively-list-directory/sol.c
@@ -4,18 +4,45 @@
 #include <stdlib.h>
 #include <dirent.h>
 
-void list_dir_flat(const char * path) {
+void list_dir_flat(const char * pathname) {
     DIR *dp;
 
     struct dirent *dirp;
 
-    if ((dp = opendir(path)) == NULL) {
+    if ((dp = opendir(pathname)) == NULL) {
         printf("Cannot open directory. Error: %s\n", strerror(errno));
         exit(1);
     }
 
     while((dirp = readdir(dp)) !=  NULL) {
         printf("%s ", dirp->d_name);
+    }
+
+    closedir(dp);
+}
+
+void list_dir_recur(const char * pathname) {
+    DIR *dp;
+    struct dirent *dirp;
+    char PATH[259];
+    
+    memset(PATH, 0, sizeof PATH);
+
+    if ((dp = opendir(pathname)) == NULL) {
+        return;
+    }
+
+    while((dirp = readdir(dp)) !=  NULL) {
+        if (dirp->d_type == DT_DIR) {
+            if (strcmp(dirp->d_name, ".") == 0 || strcmp(dirp->d_name, "..") == 0) {
+                continue;
+            }
+            printf("%s/%s\n", pathname, dirp->d_name);
+            snprintf(PATH, sizeof(PATH) - 1, "%s/%s", pathname, dirp->d_name);
+            list_dir_recur(PATH);
+        } else {
+            printf("%s/%s\n", pathname, dirp->d_name);
+        }
     }
 
     closedir(dp);
@@ -31,7 +58,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    list_dir_flat(argv[1]);
+    list_dir_recur(argv[1]);
 
     return 0;
 }
